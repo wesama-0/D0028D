9e90de9b7ac17ca986688f3ee31c4cc83f5d92ab
wesama-0 <wesama-0@student.ltu.se>
2026-02-19 15:04:43 +0100

    Script changes1

--- w8_snmp_poller/poller.py
+++ w8_snmp_poller/poller.py
@@ -5,19 +5,45 @@ import logging
 import subprocess
 import yaml
 
-def load_config(file): #reading the yaml file
-    with open(file, "r", encoding="utf-8") as yml:
-        yaml_config = yaml.safe_load(yml)
+def load_config(path): #reading the yaml file
+    with open(path, "r", encoding="utf-8") as yml:
+        cfg = yaml.safe_load(yml)
 
-    if yaml_config is None:
+    if cfg is None:
         raise ValueError("Config file is empty")
 
-    return yaml_config
+    return cfg
 
-def validate_config():
+def validate_config(cfg):
+    if not isinstance(cfg, dict) : #if it is not a dictionary
+        raise ValueError("Config must be a dictionary")
 
+    if "targets" not in cfg:
+        raise ValueError("targets are missing")
 
-#def merge_defaults():
+    if "defaults" not in cfg:
+        raise ValueError("defaults are missing")
+
+    value_defaults = cfg["defaults"]
+    value_targets = cfg["targets"]
+
+    if not isinstance(value_defaults, dict):
+        raise ValueError("default is not a directory")
+
+    if not isinstance(value_targets, list):
+        raise ValueError("targets is not a list")
+
+    for target in value_targets:
+        if not isinstance(target, dict):
+            raise ValueError("target is no a dict")
+
+    if "name" not in target:
+        raise ValueError("name is not in target")
+
+    if "ip" not in target:
+        raise ValueError("ip is not in target")
+
+#def merge_defaults(defaults, target):
 
 
 #def build_snmpget_cmd():
