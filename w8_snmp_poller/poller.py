74bd6d93ab68b5f1bf94d43c0396740121485852
wesama-0 <wesama-0@student.ltu.se>
2026-02-21 19:41:51 +0100

    validate_config done

--- w8_snmp_poller/poller.py
+++ w8_snmp_poller/poller.py
@@ -38,28 +38,44 @@ def validate_config(cfg):
     if not isinstance(default_oids, list) or len(default_oids) == 0:
         raise ValueError("defaults.oids must be a non-empty list")
 
+    for oid in default_oids:
+        if not isinstance(oid, str):
+            raise ValueError("oid is not a string")
+        if len(oid) == 0:
+            raise ValueError("oid is empty")
+
     if not isinstance(value_targets, list):
         raise ValueError("targets is not a list")
 
     if len(value_targets) == 0:
         raise ValueError("List is empty")
 
-    required_targets = ["ip", "name"]
+    required_field = ["ip", "name"]
 
-    for target in required_targets:
-        if target not in value_targets:
-            raise ValueError(f"{target} is missing in target")
+    for target in value_targets:
+        if not isinstance(target, dict): #controlls that target is a dict
+                raise ValueError(f"{target} is not a dict")
+        if "oids" in target:
+            if not isinstance(target["oids"], list):
+                raise ValueError("oids is not a list")
+            if len(target["oids"]) == 0:
+                raise ValueError("the list of oids is empty")
+            for oid in target["oids"]:
+                 if not isinstance(oid, str):
+                    raise ValueError("oids is not a string")
+                 if oid == "":
+                      raise ValueError("oids is empty")
+                     
+        for field in required_field: #controls requierd fields
+            if field not in target:
+                raise ValueError(f"{field} is missing in target")
+            if not isinstance(target[field], str):
+                raise ValueError (f"{field} is not a string!")
+            if target[field] == "":
+                raise ValueError (f"{field} is empty!")
+            
 
-target in value_targets:
-        if not isinstance(target, dict):
-            raise ValueError("target is no a dict")
 
-        if not isinstance(target["name"], str) or target["name"] == "":
-            raise ValueError("name must be a non-empty string")
-
-        if not isinstance(target["ip"], str) or target["ip"] == "":
-            raise ValueError("ip must be a non-empty string")
-        for target in 
 print(validate_config(load_config("config.yml")))
 
 #def merge_defaults(defaults, target):
